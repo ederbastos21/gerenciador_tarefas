807d9caeaface7c7e082c45c7e6a3024a4002618
eder <eder.bastos21@gmail.com>
2025-06-11 00:34:49 -0300

    adicionar componente ListaTarefas

---
+++ src/components/ListaTarefas/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styles'
+
+const ListaTarefas = () => {
+  return (
+    <Container>
+      <p>Nenhuma tarefa adicionada ainda.</p>
+    </Container>
+  )
+}
+
+export default ListaTarefas
---
+++ src/components/ListaTarefas/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  display: grid;
+  padding: 2rem;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+  max-width: 800px;
+  margin: 0 auto;
+`
