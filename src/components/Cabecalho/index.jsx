31e2af7286d418998b6d2dfa2c1a7db37945a224
eder <eder.bastos21@gmail.com>
2025-06-11 00:31:30 -0300

    criar pasta components e o cabecalho

---
+++ src/components/Cabecalho/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styles'
+
+const cabecalho = () => {
+  return (
+    <Container>
+      <h1>Gerenciador de Tarefas</h1>
+    </Container>
+  )
+}
+
+export default Cabecalho
---
+++ src/components/Cabecalho/styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  background: #0077cc;
+  color: white;
+  padding: 2rem;
+  text-align: center;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+`
